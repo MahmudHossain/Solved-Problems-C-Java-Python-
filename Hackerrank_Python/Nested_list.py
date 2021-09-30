11593d9af0a522418196694f2587ecf34dbfa674
MahmudHossain <bmahmudcse@gmail.com>
2021-09-30 20:37:00 +0600

    Added nested_list.py from hackerrank

---
+++ Hackerrank_Python/Nested_list.py
@@ -0,0 +1,20 @@
+l = []
+second_lowest_names = []
+scores = set()
+
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    l.append([name, score])
+    scores.add(score)
+#print(l)
+#print(scores)
+second_lowest = sorted(scores)[1]#second_lowest score
+#print(second_lowest)
+
+for name, score in l:
+    if score == second_lowest:
+        second_lowest_names.append(name)
+
+for name in sorted(second_lowest_names):
+    print(name, end='\n')
